16bbb8ed590841528b95e2f8387bc70698ad8f5f
Thiago <thiago.dasilva@inbrace.com>
2024-06-25 19:58:33 -0400

    Fix tests

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock
-from src.services import Validators, Service
+from src.services import Validators, Handler
 from src.exc import InvalidEmailError, PersistenceError, QueueInteractionError
 
 
@@ -21,12 +21,12 @@ class TestValidators(unittest.TestCase):
         self.assertEqual(raised, False)
 
 
-class TestService(unittest.TestCase):
+class TestHandler(unittest.TestCase):
     def setUp(self):
         self.validators = Mock()
         self.email_dal = Mock()
         self.queue_service = Mock()
-        self.service = Service(
+        self.handler = Handler(
             validators=self.validators,
             email_dal=self.email_dal,
             queue_service=self.queue_service
@@ -34,59 +34,55 @@ class TestService(unittest.TestCase):
 
     def test_call_validators(self):
         event = {'email': 'invalidemail'}
-        self.service.handle(event=event)
+        self.handler.handle(data=event)
         self.validators.validate_email.assert_called_once_with(event['email'])
 
     def test_validation_failure(self):
         event = {'email': 'invalidemail'}
         error_message = 'foo'
         self.validators.validate_email.side_effect = InvalidEmailError(error_message)
-        res = self.service.handle(event=event)
+        res = self.handler.handle(data=event)
         expected_res = {
                 "message": error_message,
-                "status_code": 400
-        }
+        }, 400
         self.assertEqual(expected_res, res)
 
     def test_persist_email(self):
         event = {'email': 'invalidemail'}
-        self.service.handle(event=event)
+        self.handler.handle(data=event)
         self.email_dal.save.assert_called_once_with(event['email'])
 
     def test_persist_email_failure(self):
         event = {'email': 'invalidemail'}
         error_message = 'failed to save email'
         self.email_dal.save.side_effect = PersistenceError(error_message)
-        res = self.service.handle(event=event)
+        res = self.handler.handle(data=event)
         expected_res = {
             "message": error_message,
-            "status_code": 500
-        }
+        }, 500
         self.assertEqual(expected_res, res)
 
     def test_send_to_queue(self):
         event = {'email': 'invalidemail'}
-        self.service.handle(event=event)
+        self.handler.handle(data=event)
         self.queue_service.add_to_queue.assert_called_once_with(event['email'])
 
     def test_send_to_queue_failure(self):
         event = {'email': 'invalidemail'}
         error_message = 'failed to send email to queue'
         self.queue_service.add_to_queue.side_effect = QueueInteractionError(error_message)
-        res = self.service.handle(event=event)
+        res = self.handler.handle(data=event)
         expected_res = {
             "message": error_message,
-            "status_code": 500
-        }
+        }, 500
         self.assertEqual(expected_res, res)
 
     def test_success(self):
         event = {'email': 'invalidemail'}
-        res = self.service.handle(event=event)
+        res = self.handler.handle(data=event)
         expected_res = {
             "message": f"Successfully received contact request. You should receive an email shortly on {event['email']} with my contact information.",
-            "status_code": 200
-        }
+        }, 200
         self.assertEqual(expected_res, res)
 
 
