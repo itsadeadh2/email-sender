5fdd22c08139dffa85843e7c11b2381eea475ffe
Thiago <thiago.dasilva@inbrace.com>
2024-06-26 18:15:18 -0400

    Change to ECS

--- src/app.py
+++ src/app.py
@@ -5,41 +5,43 @@ from flask_smorest import Blueprint
 from flask.views import MethodView
 from src.services import QueueService, EmailDAL, Handler, Validators
 
-app = Flask(__name__)
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+def create_app():
+    app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-api = Api(app)
-
-
-blp = Blueprint("email", "email", description="Operations on emails")
-@blp.route('/email')
-class Email(MethodView):
-    def __init__(self):
-        self.handler = Handler(
-            validators=Validators,
-            email_dal=EmailDAL(),
-            queue_service=QueueService()
-        )
-
-    def post(self):
-        body = request.get_json()
-        data, status = self.handler.handle(body)
-        return data, status
-
-    def get(self):
-        print("test")
-        return {"emails": []}, 200
+    api = Api(app)
 
 
+    blp = Blueprint("email", "email", description="Operations on emails")
+    @blp.route('/email')
+    class Email(MethodView):
+        def __init__(self):
+            self.handler = Handler(
+                validators=Validators,
+                email_dal=EmailDAL(),
+                queue_service=QueueService()
+            )
 
-api.register_blueprint(blp)
+        def post(self):
+            body = request.get_json()
+            data, status = self.handler.handle(body)
+            return data, status
+
+        def get(self):
+            print("test")
+            return {"emails": []}, 200
+
+    api.register_blueprint(blp)
+
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='localhost', port=3000)
