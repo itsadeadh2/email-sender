ec1a410211993797dca8e8a7e18d16136bc1a695
Thiago <thiago.dasilva@inbrace.com>
2024-06-25 20:06:53 -0400

    Add app metadata

--- src/app.py
+++ src/app.py
@@ -6,6 +6,13 @@ from flask.views import MethodView
 from services import QueueService, EmailDAL, Handler, Validators
 
 app = Flask(__name__)
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
 
@@ -26,8 +33,13 @@ class Email(MethodView):
         return data, status
 
     def get(self):
+        print("test")
         return {"emails": []}, 200
 
 
 
 api.register_blueprint(blp)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000)
