35ef27940159a95d303c2dc4d395ce9a97868d1d
Thiago <thiago.dasilva@inbrace.com>
2024-06-27 16:57:00 -0400

    Fix env grabbing, remove unnecessary docs

--- src/services/queue.py
+++ src/services/queue.py
@@ -7,7 +7,7 @@ from src.exc import QueueInteractionError
 class Queue:
     def __init__(self, queue_url, sqs=None):
         self.sqs = sqs or boto3.client('sqs')
-        self.queue_url = os.getenv(queue_url)
+        self.queue_url = queue_url
 
     def add_to_queue(self, email):
         try:
